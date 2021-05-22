d1862660b9dc28c3d2edd486784c93d59782c855
hvlck <just.ethan@protonmail.com>
2021-05-22 15:09:35 -0400

    chore: set up static assets

---
+++ static/build.ts
@@ -0,0 +1,24 @@
+require("esbuild")
+    .build({
+        entryPoints: ["./src/scripts/index.ts"],
+        bundle: true,
+        outfile: "dist/index.js",
+        minify: true,
+        sourcemap: true,
+        treeShaking: true,
+        watch: true,
+        keepNames: true,
+    })
+    .catch(() => process.exit(1));
+
+require("esbuild")
+    .build({
+        entryPoints: ["./src/styles/index.css"],
+        bundle: true,
+        outdir: "dist/",
+        minify: true,
+        treeShaking: true,
+        watch: true,
+        keepNames: true,
+    })
+    .catch(() => process.exit(1));
