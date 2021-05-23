96841711de8eca440add78bd860e3ac72a86c68c
hvlck <just.ethan@protonmail.com>
2021-05-22 20:33:45 -0400

    chore: change storage backend to text file

--- crates/api/src/lib.rs
+++ crates/api/src/lib.rs
@@ -1,8 +1,18 @@
-use util::LinkRequest;
+use std::fmt::{write, Display};
+
 use store::store_link;
+use util::LinkRequest;
 use views::{DeleteTemplate, IndexTemplate};
 
-use actix_web::{get, post, web::Json, HttpResponse, Responder, Result};
+use actix_web::{
+    dev::HttpResponseBuilder,
+    get,
+    http::{header, StatusCode},
+    post,
+    web::Json,
+    HttpResponse, Responder, ResponseError, Result,
+};
+use serde::{Deserialize, Serialize};
 
 /// Serves the root index page
 #[get("/")]
@@ -10,10 +20,63 @@ pub async fn serve_root() -> impl Responder {
     IndexTemplate {}
 }
 
+#[derive(Serialize, Deserialize, Clone)]
+struct NewLinkResponse {
+    status: String,
+    shortlink: String,
+}
+
+#[derive(Debug)]
+pub enum NewError {
+    InvalidForm,
+    AlreadyExists,
+    SomethingWentWrong,
+}
+
+impl Display for NewError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::InvalidForm => write!(f, "Invalid form information was sent"),
+            Self::AlreadyExists => write!(f, "This link already exists!"),
+            Self::SomethingWentWrong => write!(f, "Something went wrong on our end"),
+        }
+    }
+}
+
+impl actix_web::error::ResponseError for NewError {
+    fn error_response(&self) -> HttpResponse {
+        match self {
+            NewError::InvalidForm => HttpResponse::BadRequest().finish(),
+            NewError::AlreadyExists => HttpResponse::Conflict().finish(),
+            NewError::SomethingWentWrong => HttpResponse::InternalServerError().finish(),
+        }
+    }
+
+    fn status_code(&self) -> StatusCode {
+        match self {
+            NewError::InvalidForm => StatusCode::BAD_REQUEST,
+            NewError::AlreadyExists => StatusCode::CONFLICT,
+            NewError::SomethingWentWrong => StatusCode::INTERNAL_SERVER_ERROR,
+        }
+    }
+}
+
 /// API endpoint for generating a new shortlink.
 #[post("/new")]
-pub async fn serve_new_api(data: Json<LinkRequest>) -> impl Responder {
-    "not implemented"
+pub async fn serve_new_api(data: Json<LinkRequest>) -> Result<impl Responder, NewError> {
+    let form = data.into_inner();
+    match store_link(&form) {
+        Ok(()) => Ok(HttpResponse::Ok().json(NewLinkResponse {
+            status: String::from("ok"),
+            shortlink: String::from(form.id),
+        })),
+        Err(error) => match error {
+            store::SaveError::AlreadyExists => Err(NewError::AlreadyExists),
+            store::SaveError::OpenFailure => Err(NewError::SomethingWentWrong),
+            store::SaveError::WriteFailure => Err(NewError::SomethingWentWrong),
+            store::SaveError::Other => Err(NewError::SomethingWentWrong),
+        },
+    }
 }
 
 /// Serves the delete page
--- crates/store/src/lib.rs
+++ crates/store/src/lib.rs
@@ -1,6 +1,11 @@
-use util::LinkRequest;
-use sled::Config;
+use std::{
+    fs::{read_to_string, write},
+    path::PathBuf,
+};
 
+use util::LinkRequest;
+
+#[derive(Debug)]
 pub enum SaveError {
     AlreadyExists,
     OpenFailure,
@@ -18,49 +23,65 @@ impl SaveError {
     }
 }
 
-pub fn store_link(link: LinkRequest) -> Result<(), SaveError> {
-    let config = Config::default().path("./links");
-
-    if let Ok(db) = config.open() {
-        if let Ok(_) = db.get(link.id.to_owned()) {
-            Err(SaveError::AlreadyExists)
+fn already_exists(link: &String, file: String) -> Result<bool, ()> {
+    let mut has_link: bool = false;
+    for x in file.lines() {
+        let (id, misc) = x.split_at(x.find('.').unwrap());
+        if String::from(id) == link.to_owned() {
+            has_link = true;
+            break;
         } else {
-            if let Ok(_) = db.insert(link.id.as_bytes(), link.url.to_owned().as_bytes()) {
-                match store_code(&link.id, link.passcode) {
-                    Ok(()) => Ok(()),
-                    Err(error) => Err(error),
+            continue;
+        };
+    }
+
+    Ok(has_link)
+}
+
+pub const STORAGE_LOCATION: &'static str = env!("STORAGE");
+
+pub fn store_link(link: &LinkRequest) -> Result<(), SaveError> {
+    match read_to_string(STORAGE_LOCATION) {
+        Ok(file) => {
+            if let Ok(does_exist) = already_exists(&link.id, file.clone()) {
+                if does_exist == true {
+                    Err(SaveError::AlreadyExists)
+                } else {
+                    if let Ok(()) = write(
+                        STORAGE_LOCATION,
+                        format!(
+                            "{}\n{}.{}.{}",
+                            file,
+                            link.id,
+                            link.url.replace(".", "!"),
+                            link.passcode
+                        ),
+                    ) {
+                        Ok(())
+                    } else {
+                        Err(SaveError::WriteFailure)
+                    }
                 }
             } else {
-                Err(SaveError::WriteFailure)
+                Err(SaveError::OpenFailure)
             }
         }
-    } else {
-        Err(SaveError::OpenFailure)
+        Err(_) => Err(SaveError::OpenFailure),
     }
 }
 
-fn store_code(id: &String, code: String) -> Result<(), SaveError> {
-    let config = Config::default().path("./passcodes");
-
-    if let Ok(db) = config.open() {
-        if let Ok(v) = db.get(id) {
-            Err(SaveError::AlreadyExists)
-        } else {
-            if let Ok(_) = db.insert(id.as_bytes(), code.as_bytes()) {
-                Ok(())
-            } else {
-                Err(SaveError::WriteFailure)
-            }
-        }
-    } else {
-        Err(SaveError::OpenFailure)
-    }
-}
+// fn store_code(id: &String, code: String) -> Result<(), SaveError> {}
 
 #[cfg(test)]
 mod tests {
+    use super::*;
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn insert_link() {
+        store_link(&LinkRequest {
+            passcode: "test".to_string(),
+            id: "test".to_string(),
+            url: "https://github.com/hvlck".to_string(),
+        })
+        .unwrap();
     }
 }
