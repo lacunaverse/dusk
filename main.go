d14e5dd1a33754a1d808c8a1de825d2defa793ea
hvlck <just.ethan@protonmail.com>
2022-08-21 22:49:58 -0400

    feat: server setup, index + static asset routes

---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"database/sql"
+	_ "embed"
+	"encoding/json"
+	"errors"
+	"html/template"
+	"io"
+	"log"
+	"math/rand"
+	"net/http"
+	"net/url"
+	"time"
+
+	"github.com/gorilla/mux"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type Templates struct {
+	index  *template.Template
+	errors *template.Template
+}
+
+func (t *Templates) Render(w io.Writer, name string, data interface{}, cat string) error {
+	switch cat {
+	case "index":
+		return t.index.ExecuteTemplate(w, name, data)
+	case "errors":
+		return t.errors.ExecuteTemplate(w, name, data)
+	default:
+		return t.errors.ExecuteTemplate(w, name, data)
+	}
+}
+
+type NotFound struct {
+}
+
+func (n NotFound) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.Render(w, "404.html", "", "errors")
+}
+
+var t = &Templates{
+	index:  template.Must(template.ParseFiles("views/layout.html", "views/index.html", "views/layouts/nav.html")),
+	errors: template.Must(template.ParseFiles("views/layout.html", "views/errors/404.html", "views/layouts/nav.html")),
+}
+
+// Index root route
+func Index(w http.ResponseWriter, r *http.Request) {
+	t.Render(w, "index.html", "", "index")
+}
+func main() {
+
+	r := mux.NewRouter()
+
+	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static/dist")))).Methods("GET")
+	r.HandleFunc("/", Index).Methods("GET")
+
+	r.NotFoundHandler = NotFound{}
+	log.Fatal(http.ListenAndServe(":8000", r))
+
+}
+
+type Database struct {
+	*sql.DB
+}
+
